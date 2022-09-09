ad01027f5926d50327b83b64f061d2a709cff07c
kamolturaev <kamolturaev@gmail.com>
2022-09-09 02:41:51 -0700

    first commit

---
+++ src/app-info.js
@@ -0,0 +1,5 @@
+const appInfo = {
+    title: 'My_dashboard'
+};
+export default appInfo;
+
---
+++ src/pages/index.js
@@ -0,0 +1,4 @@
+export { default as HomePage } from './home/home';
+export { default as Page1 } from './Page1';
+export { default as ProfilePage } from './profile/profile';
+export { default as TasksPage } from './tasks/tasks';
