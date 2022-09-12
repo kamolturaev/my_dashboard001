3072f1f94250d2247fb7b52c90c9e95307372ff2
kamolturaev <kamolturaev@gmail.com>
2022-09-12 03:13:29 -0700

    First Commit

---
+++ src/pages/Page1/Page1.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Card1 from './Card1'
+export default function Page1() {
+  return (
+    <div style={{marginTop: '24'}} >
+        <Card1/>
+    </div>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
-export { default as HomePage } from './home/home';
-export { default as Page1 } from './Page1';
-export { default as ProfilePage } from './profile/profile';
-export { default as TasksPage } from './tasks/tasks';
+export { default as HomePage } from "./home/home";
+export { default as Page1 } from "./Page1/Page1";
+export { default as Page2 } from "./Page2/Page2";
+export { default as ProfilePage } from "./profile/profile";
+export { default as TasksPage } from "./tasks/tasks";
